6e4fa03b1fc50d1f275108bdee63796d9ad82572
KAVINPALANISAMY24 <kavinkumar.ct21@bitsathy.ac.in>
2023-06-02 21:59:10 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
 import './App.css';
-
+import NewsList from './Components/NewsList';
+import { browserHistory, Router, Route,Routes} 
+from 'react-router';
+import  { useEffect, useState } from 'react'
+import Home from './Components/Home';
+import About from './Components/About';
+import "bootstrap/dist/css/bootstrap.min.css";
+import Business from './Components/Business';
+import Sports from './Components/Sports';
+import Tech from './Components/Tech';
+import Health from './Components/Health';
+import Science from './Components/Science';
+import NewsList1 from './Newspaper/NewsList';
+import NewsList2 from './Newspaper1/NewsList';
+import Articles from './Components/Articles';
+import Tesla from './Components/Tesla';
+import Apple from './Components/Apple';
+import US from './Components/US';
 function App() {
+  const [input,setInput] = useState("")
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <Home />
+
+
+    <Routes>
+        <Route path="/" element={<NewsList />} />
+        <Route path="/business" element={<Business />} />
+        <Route path="/sports" element={<Sports />} />
+        <Route path="/technology" element={<Tech />} />
+        <Route path="/health" element={<Health />} />
+        <Route path="/science" element={<Science />} />
+        <Route path="/tesla" element={<Tesla />} />
+        <Route path="/apple" element={<Apple />} />
+        <Route path="/newspaper" element={<NewsList1 />} />
+        <Route path="/articles" element={<Articles />} />
+        <Route path="/usBusiness" element={<US />} />
+        <Route path="/business/newspaper1" element={<NewsList2 />} />
+
+        
+      </Routes>
+     
     </div>
   );
 }
 
 export default App;
+
+
+
+
---
+++ src/Components/Home.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Image from '../logo.png'
+import './Home.css'
+function Home() {
+  return (
+    <section>
+    <div className='header'>
+      <img id="img"src={Image} alt='Logo'/>
+
+      
+    </div>
+
+
+    </section>
+  )
+}
+
+export default  Home
